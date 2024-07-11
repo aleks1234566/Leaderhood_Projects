cecda7c5f01d6c676e693b2d6a0ab6f1ac6a32c9
aleks1234566 <aleksandre.ghomidze@gmail.com>
2024-07-11 22:32:12 +0400

    commit

---
+++ July_project/Games/2My_game.py
@@ -0,0 +1,16 @@
+import random
+from tkinter import *
+
+def next_turn():
+    pass
+
+def  check():
+    pass
+
+def  empty_spaces():
+    pass
+
+
+window = Tk()
+window.title('My project')
+window.mainloop()
